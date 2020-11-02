090ad88f6347868b83b0b8a9243aa2ade6f1d180
Craig A. McDonald <craig@mcdonald.codes>
2020-11-02 21:33:52 +0000

    Maven - initial build

---
+++ src/main/java/codes/mcdonald/craig/snakestore/SnakestoreApplication.java
@@ -0,0 +1,13 @@
+package codes.mcdonald.craig.snakestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codes/mcdonald/craig/snakestore/SnakestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.mcdonald.craig.snakestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
